341d88d70d213f585c1699eb182f271756c36fc5
Dave Winniford <dwinniford@gmail.com>
2020-03-25 16:08:47 -0500

    tests passing

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,3 +1,11 @@
 module SongsHelper
 
+    def artist_select(song, artists)
+        if song.artist.nil?
+            select_tag "song[artist_id]", options_from_collection_for_select(artists, :id, :name)
+            
+        else 
+            song.artist.name
+        end
+    end
 end
